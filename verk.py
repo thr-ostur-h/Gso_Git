b950afdbe58de780e035f0cffb05f62b6a0b8499
Þröstur Haraldsson <throsturha309@nemi.tskoli.is>
2017-01-25 16:29:49 +0000

    gerði svo að notandi getur skrifað fyrstu 3 línurnar sjálfur

--- verk.py
+++ verk.py
@@ -3,7 +3,16 @@
 
 skra=input("Sláðu inn nafn á skrá")
 nafnskra=skra + ".txt"
-skrainmin=open(nafnskra,"w+")
+skrainmin=open(nafnskra,'w+')
 
 skrainmin.write("Fyrsta linan i skranni")
 skrainmin.close()
+
+skrainmin = open(nafnskra,'w+')
+lina1 = input("Skráðu inn línu eitt")
+lina2 = input("Skráðu inn línu tvö")
+lina3 = input("Skráðu inn lína þrjú")
+skrainmin=open(nafnskra,'a')
+skrainmin.write(lina1+'\n')
+skrainmin.write(lina2+'\n')
+skrainmin.write(lina3+'\n')
